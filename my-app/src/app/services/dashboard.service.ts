aaf6afac56acff21045ea0865274978573638760
Divyani-Pandey <57456199+Divyani-Pandey@users.noreply.github.com>
2021-03-11 14:26:33 +0530

    dashboard service

--- my-app/src/app/services/dashboard.service.ts
+++ my-app/src/app/services/dashboard.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  rootURL = '/api';
+
+   get(type: string) {
+    console.log(type)
+    return this.http.post(this.rootURL + '/dashboard', {type:type});
+  }
 }
